821a1e1cc16c58d666d9dd53b01faf5f035166cd
M.Thomas <m.thomas@gso.schule.koeln>
2022-09-15 12:31:02 +0200

    Korrektur -.-

--- ausgelagerte_route.py
+++ ausgelagerte_route.py
@@ -1,31 +1,6 @@
 from flask import request
-import sqlite3
 
-def init_app(app, connection:sqlite3.Connection):
+def init_app(app):
     @app.route("/")
     def hello_world():
         return "<h1>Hello, World!</h1>"
-
-    @app.errorhandler(404)
-    def page_not_found(e):
-        return "Seite existiert nicht.", 404
-
-    @app.route('/eintragen', methods=['GET'])
-    def eintragen_get_nicht_erlaubt():
-        if not request.is_json:
-            return "Method Not Allowed", 405
-
-    @app.route('/eintragen', methods=['POST'])
-    def eintragen():
-        if not request.is_json:
-            return "Request was not JSON", 400
-
-        zitat = request.json.get('zitat')
-        quelle = request.json.get('quelle')
-
-        if not zitat:
-            return "JSON must contain zitat", 400
-        if not quelle:
-            return "JSON must contain quelle", 400
-
-        # return "Zitat eingetragen.", 200
