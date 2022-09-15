55b7955469c660b65d968d6d90f0dea1f3a370c3
M.Thomas <m.thomas@gso.schule.koeln>
2022-09-15 12:31:50 +0200

    Beispiellösungen

--- ausgelagerte_route.py
+++ ausgelagerte_route.py
@@ -1,7 +1,6 @@
 from flask import request
-import sqlite3
 
-def init_app(app, connection:sqlite3.Connection):
+def init_app(app):
     @app.route("/")
     def hello_world():
         return "<h1>Hello, World!</h1>"
@@ -20,12 +19,14 @@ def init_app(app, connection:sqlite3.Connection):
         if not request.is_json:
             return "Request was not JSON", 400
 
-        zitat = request.json.get('zitat')
-        quelle = request.json.get('quelle')
 
-        if not zitat:
-            return "JSON must contain zitat", 400
-        if not quelle:
-            return "JSON must contain quelle", 400
+# Hier steckt noch der Wurm drin...
+        # zitat = request.json.get('zitat')
+        # quelle = request.json.get('quelle')
+
+        # if not zitat:
+        #     return "JSON must contain zitat", 400
+        # if not quelle:
+        #     return "JSON must contain quelle", 400
 
         # return "Zitat eingetragen.", 200
